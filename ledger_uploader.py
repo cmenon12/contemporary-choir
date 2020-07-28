594f7d965b844803d805d532609d3c53be36f285
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2020-07-28 19:08:36 +0100

    Added connection to Drive and Sheets APIs, started working on uploading the spreadsheet to Drive.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- ledger_uploader.py
+++ ledger_uploader.py
@@ -1,6 +1,30 @@
+from __future__ import print_function
+
 import configparser
+import os.path
+import pickle
 
 from appJar import gui
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+# This variable specifies the name of a file that contains the
+# OAuth 2.0 information for this application, including its client_id
+# and client_secret.
+CLIENT_SECRETS_FILE = "credentials.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL
+# connection.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive.file"]
+
+# This file stores the user's access and refresh tokens and is created
+# automatically when the authorization flow completes for the first
+# time. This specifies where the file is stored
+TOKEN_PICKLE_FILE = "token.pickle"
 
 # Create the GUI
 APP_GUI = gui(showIcon=False)
@@ -20,15 +44,57 @@ def upload_ledger(dir_name: str):
                                         dirName=dir_name,
                                         fileTypes=[("Office Open XML Workbook", "*.xlsx")],
                                         asFile=True).name
-        with open(xlsx_filepath, "wb") as xlsx_file:
-            pass  # might need to save content
         APP_GUI.removeAllWidgets()
 
     except Exception as err:
         print("There was an error opening the spreadsheet.")
         raise SystemExit(err)
 
-    # upload as a Google Sheet
+    # Authenticate and retrieve the required services
+    services = authorize()
+
+    file_metadata = {"name": "Latest C30 Ledger",
+                     "mimeType": "application/vnd.google-apps.spreadsheet"}
+
+    media = MediaFileUpload(xlsx_filepath,
+                            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                            resumable=True)
+    file = services["drive"].files().create(body=file_metadata,
+                                            media_body=media,
+                                            fields='id').execute()
+
+
+def authorize() -> dict:
+    """Authorizes access to the user's Drive and Sheets.
+
+    :return: the authenticated services.
+    :rtype: dict
+    """
+
+    credentials = None
+    if os.path.exists(TOKEN_PICKLE_FILE):
+        with open(TOKEN_PICKLE_FILE, 'rb') as token:
+            credentials = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and \
+                credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRETS_FILE, SCOPES)
+            credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(TOKEN_PICKLE_FILE, 'wb') as token:
+            pickle.dump(credentials, token)
+
+    # Build both services and return them in a dict
+    drive_service = build('drive', 'v3', credentials=credentials,
+                          cache_discovery=False)
+    sheets_service = build('sheets', 'v4', credentials=credentials,
+                           cache_discovery=False)
+    return {"drive": drive_service, "sheets": sheets_service}
 
 
 def main():
