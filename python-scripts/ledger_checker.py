5e982cab7bc72c1491481b2d5019d18db7db6cac
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2020-08-05 21:21:35 +0100

    Updated ledger_checker.py This program can now check the ledger for any new entries via Google Sheets, and send an email with the changes. The use of an email template allows for a nicely-formatted HTML email to be sent.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- python-scripts/ledger_checker.py
+++ python-scripts/ledger_checker.py
@@ -3,12 +3,20 @@ import configparser
 import os
 import pickle
 import smtplib
-import ssl
+import socket
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 import googleapiclient
+import html2text
+from babel.numbers import format_currency
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient import errors
 from googleapiclient.discovery import build
+from jinja2 import Environment, FileSystemLoader
 from ledger_fetcher import download_pdf, convert_to_xlsx, upload_ledger
 
 __author__ = "Christopher Menon"
@@ -62,15 +70,103 @@ def authorize() -> googleapiclient.discovery.Resource:
     return apps_script_service
 
 
-def send_email(config: configparser.SectionProxy, changes: list):
-    # create email object
-    context = ssl.create_default_context()
-    server = smtplib.SMTP_SSL(config["host"], config["port"], context=context)
-    server.login(config["username"], config["password"])
-    server.sendmail(config["from"], config["to"], "this is the message. This is some <b>bold text</b>")
+def send_email(config: configparser.SectionProxy, changes: list,
+               pdf_filepath: str, url: str):
+    """Sends an email detailing the changes.
+
+    :param config: the configuration for the email
+    :type config: configparser.SectionProxy
+    :param changes: the changes made to the ledger
+    :type changes: list
+    :param pdf_filepath: the path to the PDF to attach
+    :type pdf_filepath: str
+    :param url: the URL to link to
+    :type url: str
+    """
+
+    # Create the SMTP connection
+    print("Connecting to the email server...")
+    with smtplib.SMTP(config["host"], int(config["port"])) as server:
+        server.starttls()
+        server.login(config["username"], config["password"])
+        message = MIMEMultipart("alternative")
+        message["Subject"] = config["subject"]
+        message["To"] = config["to"]
+        message["From"] = config["from"]
+
+        # Calculate the total for each cost code
+        print("Creating the email...")
+        cost_code_totals = {}
+        for item in changes:
+            if item[0] not in cost_code_totals.keys():
+                cost_code_totals[item[0]] = 0
+            if item[3] != "":
+                cost_code_totals[item[0]] += item[3]
+            else:
+                cost_code_totals[item[0]] -= item[4]
+
+        # Calculate a grand total
+        grand_total = 0
+        for key, value in cost_code_totals.items():
+            grand_total += value
+            value = format_currency(int(value), 'GBP', locale='en_GB')
+            cost_code_totals[key] = value
+        cost_code_totals["grand_total"] = format_currency(int(grand_total), 'GBP', locale='en_GB')
+
+        # Process each entry for each cost code
+        cost_codes = {}
+        for item in changes:
+            if item[3] == "":
+                item[3] = -int(item[4])
+            item[3] = format_currency(int(item[3]), 'GBP', locale='en_GB')
+            if item[0] not in cost_codes.keys():
+                cost_codes[item[0]] = []
+            cost_codes[item[0]].append(item[1:4])
+
+        # Render the template
+        root = os.path.dirname(os.path.abspath(__file__))
+        env = Environment(loader=FileSystemLoader(root))
+        template = env.get_template('email-template.html')
+        html = template.render(society_name=config["society_name"],
+                               cost_codes=cost_codes,
+                               cost_code_totals=cost_code_totals,
+                               url=url)
+
+        # Create the plain-text version of the message
+        text_maker = html2text.HTML2Text()
+        text_maker.ignore_links = True
+        text_maker.bypass_tables = False
+        text = text_maker.handle(html)
+
+        # Turn these into plain/html MIMEText objects
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(MIMEText(text, "plain"))
+        message.attach(MIMEText(html, "html"))
+
+        # Attach the ledger
+        print("Attaching the ledger...")
+        with open(pdf_filepath, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        # Add header as key/value pair to attachment part
+        filename = pdf_filepath.split("\\")[-1]
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {filename}",
+        )
+
+        # Add the attachment to message and send the message
+        message.attach(part)
+        print("Sending the email...")
+        server.sendmail(config["from"], config["to"], message.as_string())
+    print("Email sent successfully!")
 
 
-def main():
+def run_task():
     # Fetch info from the config
     parser = configparser.ConfigParser()
     parser.read("config.ini")
@@ -95,18 +191,42 @@ def main():
                                         destination_sheet_id=config["destination_sheet_id"],
                                         xlsx_filepath=xlsx_filepath)
 
+    # Connect to the Apps Script service and attempt to execute it
+    socket.setdefaulttimeout(600)
     apps_script_service = authorize()
     print("Connected to the Apps Script service.")
+    try:
+        print("Executing the Apps Script function (this may take some time)...")
+        body = {"function": config["function"], "parameters": sheet_name}
+        response = apps_script_service.scripts().run(body=body,
+                                                     scriptId=config["script_id"]).execute()
 
-    print("Executing the Apps Script function...")
-    response = apps_script_service.scripts().run(body={"function": config["function"],
-                                                       "parameters": sheet_name},
-                                                 scriptId=config["script_id"]).execute()
-    print("Apps Script function complete.")
-    changes = response["response"].get("result")
+        # Catch and print an error during execution
+        if 'error' in response:
+            error = response['error']['details'][0]
+            print("Script error message: {0}".format(error['errorMessage']))
+            if 'scriptStackTraceElements' in error:
+                # There may not be a stacktrace if the script didn't start
+                # executing.
+                print("Script error stacktrace:")
+                for trace in error['scriptStackTraceElements']:
+                    print("\t{0}: {1}".format(trace['function'],
+                                              trace['lineNumber']))
+            raise SystemExit()
 
-    send_email(config=parser["email"], changes=changes)
+        # Otherwise save the data that the Apps Script returns
+        changes = response["response"].get("result")
+
+    # Catch an error making the request to the API
+    except errors.HttpError as err:
+        raise SystemExit(err)
+
+    print("The Apps Script function executed successfully!")
+
+    send_email(config=parser["email"], changes=changes, pdf_filepath=pdf_filepath, url=new_url)
+
+    return changes
 
 
 if __name__ == "__main__":
-    main()
+    run_task()
