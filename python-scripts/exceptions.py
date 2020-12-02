938d4e73a6a0e9a6b78682484069f13f784b11cb
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2020-12-02 13:22:54 +0000

    Updated the README.md to reflect that we use multiple PDF to XLSX converters. Added brief documentation to exceptions.py.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- python-scripts/exceptions.py
+++ python-scripts/exceptions.py
@@ -1,2 +1,10 @@
+"""Custom exceptions thrown by the Python scripts."""
+
+__author__ = "Christopher Menon"
+__credits__ = "Christopher Menon"
+__license__ = "gpl-3.0"
+
+
 class ConversionTimeoutException(Exception):
+    """Thrown when the PDF to XLSX conversion times out."""
     pass
