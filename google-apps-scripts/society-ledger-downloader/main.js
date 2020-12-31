3ee99c2ffea4fb027de4492f1ff001c5f7b0a242
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2020-12-31 18:31:37 +0000

    Build the homepage for all non-Drive add-ons.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- google-apps-scripts/society-ledger-downloader/main.js
+++ google-apps-scripts/society-ledger-downloader/main.js
@@ -1,9 +1,66 @@
-function buildCommonHomePage() {
+/**
+ * Build the homepage common to all non-Drive add-ons.
+ */
+function buildCommonHomePage(e) {
+  
+  const header = CardService.newCardHeader().setTitle("Download your society ledger");
+  
+  const groupId = CardService.newTextInput()
+  .setFieldName("groupId")
+  .setTitle("Group ID")
+  .setHint("You can find this from your society admin URL (e.g. 12345 in exeterguild.org/organisation/admin/12345).")
+  .setValue(getUserProperties().groupId);
+  
+  const email = CardService.newTextInput()
+  .setFieldName("email")
+  .setTitle("Email")
+  .setValue(getUserProperties().email);
+  
+  const password = CardService.newTextInput()
+  .setFieldName("password")
+  .setTitle("Password")
+  .setValue(getUserProperties().password);
+  
+  const saveMethod = CardService.newSelectionInput()
+  .setType(CardService.SelectionInputType.RADIO_BUTTON)
+  .setTitle("How do you want to save the PDF ledger?")
+  .setFieldName("saveMethod")
+  .addItem("Save to this folder", "folder", getUserProperties().saveMethodFolder == "true")
+  .addItem("Replace this existing PDF", "replace", getUserProperties().saveMethodReplace == "true")
+  .addItem("Add a new revision to this existing PDF", "update", getUserProperties().saveMethodUpdate == "true");
+  
+  const url = CardService.newTextInput()
+  .setFieldName("url")
+  .setTitle("URL of folder or PDF file in Drive")
+  .setValue("");
+  
+  const saveDetails = CardService.newKeyValue()
+  .setBottomLabel("This will be saved for you individually.")
+    .setContent("Save for next time")
+    .setSwitch(CardService.newSwitch()
+               .setFieldName("saveDetails")
+               .setValue("true")
+               .setSelected(getUserProperties().saveDetails == "true"));
+  
+  const buttons = CardService.newButtonSet()
+  .addButton(CardService.newTextButton()
+             .setText("DOWNLOAD")
+             .setOnClickAction(CardService.newAction().setFunctionName("processSidebarForm")))
+  .addButton(CardService.newTextButton()
+             .setText("Clear saved data")
+             .setOnClickAction(CardService.newAction().setFunctionName("clearSavedData")))
+  
+  const form = CardService.newCardSection().addWidget(groupId).addWidget(email).addWidget(password)
+  .addWidget(saveMethod).addWidget(url).addWidget(saveDetails).addWidget(buttons)
+  
+  const card = CardService.newCardBuilder().setHeader(header).addSection(form);
+  
+  return card.build();
 
 }
 
 
-function buildDriveHomePage() {
+function buildDriveHomePage(e) {
 
 }
 
@@ -42,9 +99,6 @@ function saveUserProperties(data) {
 }
 
 
-
-
-
 /**
  * Gets the file ID from the URL.
  * Taken from https://stackoverflow.com/a/40324645.
@@ -75,35 +129,5 @@ function getIdFrom(url) {
  */
 function processSidebarForm(formData) {
 
-  Logger.log(`User clicked the button labelled ${formData.action}`);
-  let result = "";
-
-  // Clear the saved data
-  if (formData.action === "Clear saved data") {
-    PropertiesService.getUserProperties().deleteAllProperties();
-    result = "All saved user data deleted.";
-
-    // Runs the main script
-  } else if (formData.action === "GO!") {
-
-    // Save the preferences if requested
-    if (formData.saveDetails === "on") {
-      Logger.log("Saving form data.");
-      saveUserProperties(formData);
-    }
-
-    // Download the PDF
-    Logger.log("Downloading the PDF ledger...");
-    const pdfBlob = downloadLedger(formData);
-    Logger.log("PDF ledger downloaded successfully!");
-
-    // Save the PDF if asked
-    if (formData.savePDF === "on") {
-      const id = getIdFrom(formData.PDFLocation);
-      result = result.concat(saveToDrive(pdfBlob, id, "application/pdf"));
-    }
-
-
-  }
-  return result;
+  Logger.log(formData);
 }
