f677bf9f45ee785cd95db6fcc4bfa97b4f88aa09
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2021-04-12 19:18:11 +0100

    Started building a Google Workspace addon for The New Ledger.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

---
+++ google-apps-scripts/the-new-ledger/addon-cards.js
@@ -0,0 +1,8 @@
+/*
+  =============================================================================
+  Project Page: https://github.com/cmenon12/contemporary-choir
+  Copyright:    (c) 2020 by Christopher Menon
+  License:      GNU General Public License, version 3 (GPL-3.0)
+                http://www.opensource.org/licenses/gpl-3.0.html
+  =============================================================================
+ */
