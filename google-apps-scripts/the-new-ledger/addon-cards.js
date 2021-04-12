6537f8e1f43560b11876771c24d52bb905b74c38
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2021-04-12 21:58:01 +0100

    Copied in code from the Society Ledger Downloader.
    Also started on the homepage.
    
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- google-apps-scripts/the-new-ledger/addon-cards.js
+++ google-apps-scripts/the-new-ledger/addon-cards.js
@@ -6,3 +6,99 @@
                 http://www.opensource.org/licenses/gpl-3.0.html
   =============================================================================
  */
+
+/**
+ * Builds the homepage Card specifically for Sheets.
+ *
+ * @param {eventObject} e The event object
+ * @returns {Card} The homepage Card
+ */
+function buildSheetsHomePage(e) {
+
+  // Start to create the Card
+  const header = CardService.newCardHeader()
+    .setTitle("Process your Society Ledger")
+    .setImageUrl("https://raw.githubusercontent.com/cmenon12/contemporary-choir/main/assets/icon.png");
+  const card = CardService.newCardBuilder()
+    .setHeader(header);
+
+  return card.addSection(getOriginalSheetSection()).build();
+
+}
+
+
+/**
+ * Build and return the section with the login form.
+ * This will ensure that any undefined (or unsaved) form inputs are replaced with empty strings.
+ *
+ * @returns {CardSection} A section with the login form.
+ */
+function getOriginalSheetSection() {
+
+  let url = CardService.newTextInput()
+    .setFieldName("originalSheetURL")
+    .setTitle("Spreadsheet URL");
+
+  if (getUserProperties().originalSheetURL === undefined) {
+    url = url.setValue("");
+  } else {
+    url = url.setValue(getUserProperties().originalSheetURL);
+  }
+
+  let name = CardService.newTextInput()
+    .setFieldName("originalSheetName")
+    .setTitle("Sheet Name");
+
+  if (getUserProperties().originalSheetName === undefined) {
+    name = name.setValue("");
+  } else {
+    url = url.setValue(getUserProperties().originalSheetName);
+  }
+
+  return CardService.newCardSection()
+  .addWidget(CardService.newTextParagraph()
+    .setText("<b>The Google Sheet to compare against.</b>"))
+  .addWidget(url)
+  .addWidget(name)
+  .addWidget(CardService.newTextParagraph()
+    .setText("These details will be saved for you, and for your use only."));
+
+}
+
+
+/**
+ * Produces and displays a notification.
+ *
+ * @param {String} message The message to notify.
+ * @returns {ActionResponse} The notification to display.
+ */
+function buildNotification(message) {
+  return CardService.newActionResponseBuilder()
+    .setNotification(CardService.newNotification()
+      .setText(message))
+    .build();
+}
+
+
+/**
+ * Creates a text button for Cards that clears the saved data.
+ * @returns {TextButton} The button.
+ */
+function createClearSavedDataButton() {
+  return CardService.newTextButton()
+    .setText("Clear saved data")
+    .setOnClickAction(CardService.newAction()
+      .setFunctionName("clearSavedData"))
+}
+
+
+/**
+ * Creates the section with the standard disclaimer & attribution.
+ * @returns {CardSection} The disclaimer.
+ */
+function createDisclaimerSection() {
+  const disclaimerText = '<font color="#bdbdbd">Icon made by <a href="https://www.flaticon.com/authors/photo3idea-studio">photo3idea_studio</a> from <a href="https://www.flaticon.com/">www.flaticon.com</a>.</font>';
+  return CardService.newCardSection()
+    .addWidget(CardService.newTextParagraph()
+      .setText(disclaimerText));
+}
--- google-apps-scripts/the-new-ledger/addon-main.gs
+++ google-apps-scripts/the-new-ledger/addon-main.gs
@@ -6,3 +6,115 @@
                 http://www.opensource.org/licenses/gpl-3.0.html
   =============================================================================
  */
+
+
+/**
+ * Deletes all the saved user data and clears the form.
+ *
+ * @param {eventObject} e The event object.
+ * @returns {ActionResponse} The ActionResponse that reloads the
+ * current Card.
+ */
+function clearSavedData(e) {
+
+  PropertiesService.getUserProperties().deleteAllProperties();
+
+  // Build and return an ActionResponse
+  const navigation = CardService.newNavigation()
+    .updateCard(buildDriveHomePage(e));
+  return CardService.newActionResponseBuilder()
+    .setNavigation(navigation)
+    .build();
+}
+
+
+/**
+ * Gets and returns the user properties
+ */
+function getUserProperties() {
+  return PropertiesService.getUserProperties().getProperties();
+}
+
+
+/**
+ * Saves the user data specified as key-value pairs. These are saved to
+ * the user's properties, so they're unique to them.
+ *
+ * @param {eventObject.formInput} data The data to be saved as key-value
+ * pairs.
+ */
+function saveUserProperties(data) {
+
+  // Get the current saved properties
+  const userProperties = PropertiesService.getUserProperties();
+
+  // Save them
+  for (let prop in data) {
+    if (Object.prototype.hasOwnProperty.call(data, prop)) {
+      userProperties.setProperty(prop, data[prop]);
+    }
+  }
+}
+
+
+/**
+ *
+ * Gets the Drive file ID from the URL.
+ * Taken from https://stackoverflow.com/a/40324645.
+ *
+ * @param {String} url The URL to get the file ID from.
+ * @returns {String} The file ID.
+ */
+function getIdFrom(url) {
+  let id;
+  let parts = url.split(/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/);
+
+  if (url.indexOf('?id=') >= 0) {
+    id = (parts[6].split("=")[1]).replace("&usp", "");
+    return id;
+
+  } else {
+    id = parts[5].split("/");
+    // Using sort to get the id as it is the longest element.
+    let sortArr = id.sort(function (a, b) {
+      return b.length - a.length
+    });
+    id = sortArr[0];
+
+    return id;
+  }
+}
+
+
+/**
+ * Processes the form submission from the Sheets sidebar.
+ *
+ * @param {eventObject} e The event object.
+ * @returns {ActionResponse} The response, either a link that opens in
+ * a new tab or an error notification.
+ */
+function processSheetsSidebarForm(e) {
+
+  saveUserProperties(e.formInput);
+
+  return buildNotification(JSON.stringify(e));
+
+}
+
+
+/**
+ * Callback function for a button action. Instructs Sheets to display a
+ * permissions dialog to the user, requesting `drive.file` scope for the
+ * current file on behalf of this add-on.
+ * Also saves the form data.
+ *
+ * @param {Object} e The parameters object that contains the item's ID.
+ * @return {editorFileScopeActionResponse}
+ */
+function onRequestFileScopeButtonClicked(e) {
+
+  saveUserProperties(e.formInput)
+
+  return CardService.newEditorFileScopeActionResponseBuilder()
+      .requestFileScopeForActiveDocument().build();
+}
