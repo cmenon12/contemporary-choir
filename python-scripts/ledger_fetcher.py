f8fb08ee90ee3d00379d10b6cad8bb52a4e46abd
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2021-04-18 20:04:21 +0100

    Deleted PDFToXLSXConverter, started work on new converter.
    Using camelot-py, added converting them to CSV files.
    
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- python-scripts/ledger_fetcher.py
+++ python-scripts/ledger_fetcher.py
@@ -46,14 +46,15 @@ import configparser
 import json
 import logging
 import os
+import pathlib
 import pickle
-import random
 import time
 import traceback
 import webbrowser
 from datetime import datetime
 from typing import Optional, Union
 
+import camelot
 import pyperclip
 import requests
 from appJar import gui
@@ -222,47 +223,17 @@ class Ledger:
         :raises ConversionTimeoutError: if the conversion takes too long
         """
 
-        # Prepare for the request
-        converter = PDFToXLSXConverter(self)
-
-        # Make the request and check that it was successful
-        LOGGER.info("Sending the PDF for conversion to %s...",
-                    converter.get_name())
-        job_id = converter.upload_pdf()
-        LOGGER.info("The request was successful with no HTTP errors.")
-        LOGGER.info("The jobId is %s.", job_id)
-
-        # Prepare to keep checking the status of the conversion
-        download_url = ""
-
-        # Whilst it is still being converted
-        check_count = 0
-        while download_url == "":
-
-            # Prepare and make the request
-            LOGGER.info("Checking if the conversion is complete...")
-            download_url = converter.check_conversion_status(job_id=job_id)
-
-            # Wait before checking again
-            if download_url == "":
-                check_count += 1
-
-                # Stop if we've been waiting for 2 minutes
-                if check_count == (CONVERSION_TIMEOUT / 2):
-                    raise ConversionTimeoutError(converter.get_name(),
-                                                 CONVERSION_TIMEOUT)
-                time.sleep(2)
-
-        # Prepare and make the request to download the file
-        LOGGER.info("Downloading the converted file...")
-        xlsx_content = converter.download_xlsx(job_id=job_id,
-                                               download_url=download_url)
-
-        # Save the file
-        self.xlsx_filename = self.get_pdf_filename().replace(".pdf", ".xlsx")
-        self.xlsx_file = xlsx_content
-        if save is True:
-            self.save_xlsx()
+        # Convert the PDF to a set of CSV files
+        tables = camelot.read_pdf(self.get_pdf_filepath(),
+                                  columns=["88.2, 430.4, 504.6"],
+                                  table_areas=["27.4,755.4,568.6,30.2"],
+                                  flavor="stream",
+                                  pages="1-end")
+        path = pathlib.Path(self.dir_name, "temp-camelot")
+        path.mkdir(parents=True, exist_ok=True)
+        path = os.path.join(path, "result.csv")
+        LOGGER.info("Saving the CSV files to %s", path)
+        tables.export(path, f="csv")
 
     def update_drive_pdf(self, save: bool = True) -> None:
         """Updates the PDF in Drive with the new version of the ledger.
@@ -701,137 +672,6 @@ class Ledger:
                 LOGGER.info("Sheet %s has been un-hidden successfully.", self.sheets_data["sheet_id"])
         else:
             LOGGER.info("There is no Google Sheet to hide.")
-
-    def log(self) -> None:
-        """Logs the object to the log."""
-
-        LOGGER.info(json.dumps(self.__dict__, cls=CustomEncoder))
-
-
-class PDFToXLSXConverter:
-    """Represents a PDF to XLSX converter."""
-
-    def __init__(self, ledger: Ledger, converter_number: int = 0):
-        """Creates the converter.
-
-        :param ledger: the ledger to convert
-        :type ledger: Ledger
-        :param converter_number: the chosen converter
-        :type converter_number: int, optional
-        """
-
-        # Define the user agent, which doesn't change
-        user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75")
-
-        # Define the fixed headers
-        headers = {
-            "Connection": "keep-alive",
-            "X-Requested-With": "XMLHttpRequest",
-            "DNT": "1",
-            "User-Agent": user_agent,
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Dest": "empty",
-            "Accept-Language": "en-GB,en;q=0.9",
-        }
-
-        # If no valid number was specified then choose randomly
-        if converter_number not in range(1, NUMBER_OF_CONVERTERS + 1):
-            converter_number = random.randint(1, NUMBER_OF_CONVERTERS)
-
-        # Use pdftoexcel.com
-        if converter_number == 1:
-            self.name = "pdftoexcel.com"
-            self.request_url = "https://www.pdftoexcel.com/upload.instant.php"
-            headers["Accept"] = "*/*"
-            headers["Origin"] = "https://www.pdftoexcel.com"
-            headers["Referer"] = "https://www.pdftoexcel.com/"
-            self.files = {"Filedata": open(ledger.get_pdf_filepath(), "rb")}
-            self.status_url = "https://www.pdftoexcel.com/status"
-            self.download_url = "https://www.pdftoexcel.com"
-
-        # Use pdftoexcelconverter.net
-        else:
-            self.name = "pdftoexcelconverter.net"
-            self.request_url = "https://www.pdftoexcelconverter.net/upload.instant.php"
-            headers["Accept"] = "application/json"
-            headers["Origin"] = "https://www.pdftoexcelconverter.net"
-            headers["Referer"] = "https://www.pdftoexcelconverter.net/"
-            self.files = {"file[0]": open(ledger.get_pdf_filepath(), "rb")}
-            self.status_url = "https://www.pdftoexcelconverter.net/getIsConverted.php"
-            self.download_url = "https://www.pdftoexcelconverter.net"
-
-        # Create the session that'll be used to make all the requests
-        self.session = requests.Session()
-        self.session.headers.update(headers)
-
-        self.log()
-
-    def upload_pdf(self, raise_for_status: bool = True) -> str:
-        """Make the conversion request and upload the PDF.
-
-        :param raise_for_status: whether to raise_for_status()
-        :type raise_for_status: bool, optional
-        :return: the conversion job ID
-        :rtype: str
-        :raises HTTPError: if a bad HTTP status code is returned
-        :raises ConversionRejectedError: if server rejects the PDF
-        """
-
-        response = self.session.post(url=self.request_url, files=self.files)
-        if raise_for_status:
-            response.raise_for_status()
-        if "jobId" not in response.json().keys():
-            raise ConversionRejectedError(self.get_name())
-        return response.json()["jobId"]
-
-    def check_conversion_status(self, job_id: str,
-                                raise_for_status: bool = True) \
-            -> str:
-        """Check the status of the request and get the download URL.
-
-        :param job_id: the ID of the conversion job
-        :type job_id: str
-        :param raise_for_status: whether to raise_for_status()
-        :type raise_for_status: bool, optional
-        :return: the download URL (which might be empty)
-        :rtype: str
-        :raises HTTPError: if a bad HTTP status code is returned
-        :raises JSONDecodeError: if the response can't be decoded
-        """
-
-        response = self.session.get(url=self.status_url,
-                                    params=(("jobId", job_id), ("rand", "16")))
-        if raise_for_status:
-            response.raise_for_status()
-        return response.json()["download_url"]
-
-    def download_xlsx(self, job_id: str, download_url: str,
-                      raise_for_status: bool = True) -> bytes:
-        """Download the XLSX file.
-
-        :param job_id: the ID of the conversion job
-        :type job_id: str
-        :param download_url: the download URL
-        :type download_url: str
-        :param raise_for_status: whether to raise_for_status()
-        :type raise_for_status: bool, optional
-        :return: the XLSX file
-        :rtype: bytes
-        :raises HTTPError: if a bad HTTP status code is returned
-        """
-
-        response = self.session.get(url=self.download_url + download_url,
-                                    params={"id": job_id})
-        if raise_for_status:
-            response.raise_for_status()
-        return response.content
-
-    def get_name(self) -> str:
-        """Return the name of the converter."""
-        return self.name
 
     def log(self) -> None:
         """Logs the object to the log."""
