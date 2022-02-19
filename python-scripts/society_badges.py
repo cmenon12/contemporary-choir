8963f9f32624050ff0be6de01c8ba03fd728ae1c
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2022-02-19 17:21:12 +0000

    Added extracting each society & saving to CSV.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- python-scripts/society_badges.py
+++ python-scripts/society_badges.py
@@ -1,7 +1,13 @@
 """Quick script to get data on society badges across the Guild."""
+import csv
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
+
+# The badges, as per the text in the a.msl-groupingattributelist-link tags
+ALL_BADGES = ["Accessibility", "Democracy", "Development", "Diversity", "Fundraising", "Inclusivity", "Social",
+              "Sustainability", "welfare"]
 
 
 def main():
@@ -16,10 +22,40 @@ def main():
     print(f"We found {len(all_societies)} societies.")
     if all_societies is None:
         raise Exception("all_societies is None, the structure has changed!")
-    for child in all_societies:
-        societies[child.text] = f"https://www.exeterguild.org{child['href']}"
+    for item in all_societies:
+        societies[item.text] = f"https://www.exeterguild.org{item['href']}"
 
-    print(societies)
+    # Get the number of badges each society has
+    csv_data = []
+    for name, url in societies.items():
+        csv_data.append(get_badges_data(session, name, url))
+
+    # Save to a CSV
+    with open(f"society-badges-{datetime.now().isoformat().replace(':', '_')}.csv", "w", encoding="UTF8",
+              newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Name", "URL"] + ALL_BADGES + ["Total"])
+        writer.writerows(csv_data)
+
+
+def get_badges_data(session: requests.Session, society_name: str, society_url: str) -> list:
+    # Download it
+    badges_got = []
+    page_response = session.get(society_url)
+    page_response.raise_for_status()
+    page_soup = BeautifulSoup(page_response.text, "lxml")
+    page_badges = page_soup.find_all("a", {"class": "msl-groupingattributelist-link"})
+    for item in page_badges:
+        badges_got.append(item.text)
+
+    # Save the result
+    result = [society_name, society_url]
+    for badge in ALL_BADGES:
+        result.append(badge in badges_got)
+    result.append(len(badges_got))
+
+    print(result)
+    return result
 
 
 if __name__ == "__main__":
